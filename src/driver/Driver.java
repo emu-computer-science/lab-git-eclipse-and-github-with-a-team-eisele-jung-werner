65465b996e83f0f6120646afecef054487432174
eric-eisele <eeisele1@emich.edu>
2023-10-04 18:39:38 -0400

    Happy Class Commit

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -4,5 +4,6 @@ public class Driver {
 	public static void main(String arg[]) {
 		System.out.println("Our team is getting started.");
 		Hungry.hungry();
+		Happy.speak();
 	}
 }
---
+++ src/driver/Happy.java
@@ -0,0 +1,8 @@
+package driver;
+
+public class Happy {
+	
+	public static void speak() {
+		System.out.println("We are very happy!");
+	}
+}
