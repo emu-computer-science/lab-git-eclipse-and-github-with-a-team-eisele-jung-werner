4fe6c3b08d4853e0d229510ebc70cbba816e9c23
hjung7emu <hjung7@emich.edu>
2023-10-04 18:41:55 -0400

    Fun class

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -5,5 +5,6 @@ public class Driver {
 		System.out.println("Our team is getting started.");
 		Hungry.hungry();
 		Happy.speak();
+		Fun.Funny();
 	}
 }
---
+++ src/driver/Fun.java
@@ -0,0 +1,7 @@
+package driver;
+
+public class Fun {
+	public static void Funny() {
+		System.out.println("This is fun!");
+	}
+}
